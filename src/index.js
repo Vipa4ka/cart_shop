169e382708e09b81a2eba2138b74b276bb053a2e
Vipa4ka <vipa4ka@gmail.com>
2022-12-15 22:42:09 +0200

    init: start project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/index.scss';
 
+cart = {
+  item: [],
+  getItems() {},
+  add(product) {},
+  remove(productName) {},
+  clear(product) {},
+  countTotalPrice() {},
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
 
+cart.add({ name: 'apple', price: 50 });
+cart.add({ name: 'lemon', price: 70 });
+cart.add({ name: 'cherry', price: 100 });
+cart.add({ name: 'strawberry', price: 150 });
