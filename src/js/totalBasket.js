ff6868774501422ebaea643266cd3f4e0abd2d4e
Vipa4ka <vipa4ka@gmail.com>
2023-01-29 18:00:30 +0200

    fix: quantity product

--- src/js/totalBasket.js
+++ src/js/totalBasket.js
@@ -23,54 +23,22 @@ function onClickProgress(e) {
   const sumProd = listProd.querySelector('.sum-basket');
   const isButtonMinus = e.target.matches('.minus');
   const isButtonPlus = e.target.matches('.plus');
+  const quantityProd = listProd.querySelector('.quantity');
 
   if (isButtonPlus) {
     quantityCart = cart.increaseQuantity(nameProd);
+    quantityProd.innerHTML = quantityCart;
   }
 
   if (isButtonMinus) {
     quantityCart = cart.decreaseQuantity(nameProd);
+    quantityProd.innerHTML = quantityCart;
   }
   sumProd.innerHTML = cart.amountProd(nameProd);
-  // quantityProd.innerHTML = quantityCart;
+
   refs.totalSum.innerHTML = cart.countTotalPrice();
 }
 
 function renderCards(arrayCart) {
   return arrayCart.map(cardBasket).join('');
 }
-
-// ______________________________________________________________
-
-// refs.modalElem.addEventListener('click', onClickBasket);
-
-// function onClickBasket(e) {
-//   refs.totalSum.innerHTML = cart.countTotalPrice();
-//   refs.modalBasket.addEventListener('click', onClickOnKg);
-//   const listCards = renderCards(cart.items);
-//   const list = document.querySelector('.modal-basket');
-//   list.insertAdjacentHTML('afterbegin', listCards);
-// }
-
-// function onClickOnKg(e) {
-//   const listProd = e.target.closest('.list-card-basket');
-//   const nameProd = listProd.querySelector('.name-product-basket').innerHTML;
-//   const quantityProd = listProd.querySelector('.quantity');
-//   const sumProd = listProd.querySelector('.sum-basket');
-//   const priceProd = listProd.querySelector('.price-basket');
-
-//   const isButtonMini = e.target.matches('.minus');
-
-//   if (!isButtonMini) {
-//     quantityCart = cart.increaseQuantity(nameProd);
-//   } else {
-//     quantityCart = cart.decreaseQuantity(nameProd);
-//   }
-//   sumProd.innerHTML = cart.amountProd(nameProd);
-//   quantityProd.innerHTML = quantityCart;
-//   refs.totalSum.innerHTML = cart.countTotalPrice();
-// }
-
-// function renderCards(arrayCart) {
-//   return arrayCart.map(cardBasket).join('');
-// }
